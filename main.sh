6451698948ced70a4c61b6858c2f583141ed5e6c
riyadrb <sailor.ryd@gmail.com>
2024-01-11 23:59:22 +0600

    Installing composer and laravel modified

--- main.sh
+++ main.sh
@@ -20,8 +20,8 @@ read -p "Port Number: " LISTEN_PORT
 mkdir -p "/var/www/$PROJECT_NAME"
 cd "/var/www/$PROJECT_NAME"
 
-#<<<-------------------------------------------------------------------------------------------------------------->>>
 
+#<<<-------------------------------------------------------------------------------------------------------------->>>
 
 required_commands=("git" "curl" "unzip" "expect" "jq")
 
@@ -37,36 +37,27 @@ git clone $GITHUB_URL . || { echo "Failed to Clone Repository. Exiting"; exit 1;
 
 
 #<<<-------------------------------------------------------------------------------------------------------------->>>
-
-
 # Installing composer and laravel
 function install_laravel() {
-    if ! command_exists composer &> /dev/null; 
-    then
+    if ! command_exists composer &> /dev/null; then
         # Installing composer
-        if ! command_exists composer; 
-        then
-            echo "Composer is not installed"
-            sudo apt install php-cli php-mbstring
-            curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-        fi
-            if ! command_exists composer &> /dev/null; 
-            then
-                >&2 echo "ERROR: Composer installation failed"
-                exit 1
-            fi
+        echo "Installing composer..."
+        sudo apt install php-cli php-mbstring
+        curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
+        if ! command_exists composer &> /dev/null; then
+             echo "ERROR: Composer installation failed" >&2
+            exit 1
+        fi
 
         echo "Composer has been installed successfully."
     else
         echo "Composer is already installed."
     fi
 
-
     composer global require "laravel/installer"
     
-    if [ $? -eq 0 ]; 
-    then
+    if [[ $? == 0 ]]; then
         echo "Laravel has been installed globally."
     else
         echo "Failed to install Laravel globally. Please check logs for more details."
@@ -80,8 +71,7 @@ install_laravel
 #<<<-------------------------------------------------------------------------------------------------------------->>>
 
 # Installing Nginx
-if ! command_exists nginx; 
-then 
+if ! command_exists nginx; then
     echo "Nginx Installing"
     sudo apt update
     sudo apt install nginx -y
@@ -114,8 +104,7 @@ function install_php_and_libraries() {
     php${PHP_VERSION}-gettext \
     php${PHP_VERSION}-dom     
 
-    if [ $? -eq 0 ]; 
-    then
+    if [[ $? == 0 ]]; then
         echo "PHP ${PHP_VERSION} and required libraries have been successfully installed."
     else
         echo "Failed to install PHP ${PHP_VERSION} and libraries. Please check logs for more details."
@@ -126,11 +115,9 @@ function install_php_and_libraries() {
 
 # Switching php version
 function switch_php_version() {
-    if [ "$1" == "7" ]; 
-    then
+    if [[ "$1" == "7" ]]; then
         sudo update-alternatives --set php /usr/bin/php${PHP_VERSION}
-    elif [ "$1" == "8" ]; 
-    then
+    elif [[ "$1" == "8" ]]; then
         sudo update-alternatives --set php /usr/bin/php${PHP_VERSION}
     else
         echo "Unsupported PHP Version."
@@ -145,40 +132,31 @@ function switch_php_version() {
 function get_laravel_php_version() {
     LARAVEL_PATH="/var/www/$PROJECT_NAME"  
     #<<<------------------------------------------------>>>
-    if [ -f "${LARAVEL_PATH}/composer.json" ]; 
-    then
-
+    if [[ -f "${LARAVEL_PATH}/composer.json" ]]; then
         # jq json perser tool
         PHP_VERSION=$(jq -r '.require.php' "${LARAVEL_PATH}/composer.json" | sed -E 's/\^([0-9]+\.[0-9]+)/\1/g' | tr '|' '\n' | sort -Vr | head -n1)
-
         echo "Detected PHP Version: php${PHP_VERSION}"
 
             # Checking if the Detected PHP Version is below 7.4 and setting it to 7.4
-            if [[ "$(echo -e "7.4\n${PHP_VERSION}" | sort -V | head -n1)" != "7.4" ]]; 
-            then
+            if [[ "$(echo -e "7.4\n${PHP_VERSION}" | sort -V | head -n1)" != "7.4" ]]; then
                 PHP_VERSION="7.4"
-            elif [[ "$PHP_VERSION" == "8.0" ]]; 
-            then
+            elif [[ "$PHP_VERSION" == "8.0" ]]; then
                 PHP_VERSION="8.1"
             fi
-
             echo "Adjusted PHP Version:php${PHP_VERSION}"
 
 
 
-        #<<<------------------------------------------------>>>
-        if [ "$PHP_VERSION" != "null" ]; 
-        then
+        #<<<----------------------------------------------------->>>
+        if [[ "$PHP_VERSION" != "null" ]]; then
             echo "Laravel app requires PHP version: php${PHP_VERSION}"
             PHP_MAJOR_VERSION=$(echo "$PHP_VERSION" | sed 's/\^//' | cut -d '.' -f 1)
 
 
             #<<<------------------------------------------------>>>
             # Check for supported PHP versions
-            if [[ "$PHP_MAJOR_VERSION" == "7" ]]; 
-            then
-                if ! command_exists "php${PHP_VERSION}"; 
-                then
+            if [[ "$PHP_MAJOR_VERSION" == "7" ]]; then
+                if ! command_exists "php${PHP_VERSION}"; then
                     sudo add-apt-repository ppa:ondrej/php -y   #Added Ondrej repo as ubuntu 22.04's default does not have php7.4
                     sudo apt update
                     echo "PHP "$PHP_VERSION" is not installed. Installing..."
@@ -189,10 +167,8 @@ function get_laravel_php_version() {
                 # Calling Function with param
                 switch_php_version "$PHP_MAJOR_VERSION"
 
-            elif [[ "$PHP_MAJOR_VERSION" == "8" ]]; 
-            then
-                if ! command_exists "php${PHP_VERSION}"; 
-                then
+            elif [[ "$PHP_MAJOR_VERSION" == "8" ]]; then
+                if ! command_exists "php${PHP_VERSION}"; then
                     echo "PHP "$PHP_VERSION" is not installed. Installing..."
                     sudo apt install "php${PHP_VERSION}" "php${PHP_VERSION}"-fpm -y
                 fi
@@ -248,14 +224,11 @@ sed -i -e "s|^DB_DATABASE=.*|DB_DATABASE=\"$PROJECT_NAME\"|" \
 
 
 # Check if MySQL is installed
-if ! command_exists mysql; 
-then 
+if ! command_exists mysql; then
     echo "MySQL is not installed"
     sudo apt install mysql-server-8.0 -y
-
     # Set up MySQL initially with root password
     sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$DB_PASSWORD';"
-
     # Restart MySQL service to apply changes
     sudo systemctl restart mysql.service
 fi
@@ -264,10 +237,8 @@ fi
 mysql -u "$DB_USERNAME" -p"$DB_PASSWORD" -e "SELECT 1;"  # This line will authenticate and check if MySQL is accessible
 
 # Check the exit status of the above command to ensure successful authentication
-if [ $? -eq 0 ]; 
-then
+if [[ $? == 0 ]]; then
     echo "MySQL setup successful. Proceeding with database operations..."
-    
     # Create database if it doesn't exist
     mysql -u "$DB_USERNAME" -p"$DB_PASSWORD" -e"CREATE DATABASE IF NOT EXISTS $PROJECT_NAME;" || { echo "Faild to Create Mysql Database. Exiting."; exit 1; }
     
